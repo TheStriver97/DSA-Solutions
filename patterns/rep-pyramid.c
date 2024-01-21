8c7e4f916118a074723be896b2e0bdae68fc4072
TheStriver97 <nileshh97@proton.me>
2024-01-21 09:25:39 +0530

    added solution to the pattern 4

--- patterns/rep-pyramid.c
+++ patterns/rep-pyramid.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(){
     //initializing a variable N to iterate the loop N number of times
-    int N = -5;
+    int N =5;
     //the outer for loop with variable i controls the number of columns
     if (N<0)
     {printf("N should be a valid positive integer only.\n");
     return 1;}
     else
-    {for (int i=1;i<N;i++){
-        //the inner for loop with variable j controls the number being printed in each row, which increases by 1 in every iteration and stops as the value of j equates to the value of the outer loop's variable i.
+    {for (int i=1;i<=N;i++){
+        //the inner for loop with variable j controls the number being printed in each row, which increases by 1 in every iteration and stops as the value of i equates to the value of the variable N.
         for(int j = 1;j<=i;j++){
-            //printing the value of variable j for showing an output on console
-            printf("%d",j);
+            //printing the value of variable i for showing an output on console
+            printf("%d ",i);
         } 
         // to enter a new line as the loop completes one iteration.
         printf("\n");
