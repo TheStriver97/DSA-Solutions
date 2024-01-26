7ec9fde9c11cbc2d69e76ab7bd3103beea1681a6
TheStriver97 <nileshh97@proton.me>
2024-01-26 12:50:50 +0530

    solution for pattern11 added

---
+++ patterns/p11.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void pattern11(int n){
+    int start =1;
+    for (int i=0;i<n;i++)
+    {
+        if(i%2 ==0) start =1;
+        else start=0;
+        for (int j=0;j<i;j++)
+        {
+            printf("%d ",start);
+            start = 1-start;
+        }
+    
+        printf("\n");
+    }
+}
+
+int main()
+{   int n = 5;
+    pattern11(n);
+    return 0;
+}
